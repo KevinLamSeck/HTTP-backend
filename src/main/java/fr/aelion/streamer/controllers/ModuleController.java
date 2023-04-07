9a8f9cf87e580fbb6100d5b1765480af46e22447
elsa <elsa-david@laposte.net>
2023-04-07 16:43:48 +0200

    Implemented Module Controller and Service with a findAll, findOne et Add Module

---
+++ src/main/java/fr/aelion/streamer/controllers/ModuleController.java
@@ -0,0 +1,44 @@
+package fr.aelion.streamer.controllers;
+
+import fr.aelion.streamer.dto.CourseAddDto;
+import fr.aelion.streamer.dto.FullCourseDto;
+import fr.aelion.streamer.dto.conceptorDtos.MediaDto;
+import fr.aelion.streamer.dto.conceptorDtos.ModuleDto;
+import fr.aelion.streamer.services.MediaService;
+import fr.aelion.streamer.services.ModuleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("api/v1/modules") // http://127.0.0.1:8080/api/v1/students
+@CrossOrigin
+public class ModuleController {
+
+    @Autowired
+    private ModuleService moduleService;
+
+    @GetMapping
+    public List<ModuleDto> findAll() {
+        return moduleService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> findOne(@PathVariable int id) {
+        try {
+            return ResponseEntity.ok(moduleService.findOne(id));
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>( "Module with " + id + " was not found", HttpStatus.NOT_FOUND);
+        }}
+
+    @PostMapping
+    public ResponseEntity<ModuleDto> add(@RequestBody ModuleDto module) {
+        ModuleDto moduleDto = this.moduleService.add(module);
+        return ResponseEntity.ok(moduleDto);
+    }
+
+}
--- src/main/java/fr/aelion/streamer/services/ModuleService.java
+++ src/main/java/fr/aelion/streamer/services/ModuleService.java
@@ -1,7 +1,17 @@
 package fr.aelion.streamer.services;
 
+import fr.aelion.streamer.dto.FullCourseDto;
+import fr.aelion.streamer.dto.conceptorDtos.MediaDto;
+import fr.aelion.streamer.dto.conceptorDtos.ModuleDto;
+import fr.aelion.streamer.entities.CourseToModule;
 import fr.aelion.streamer.entities.Media;
+import fr.aelion.streamer.entities.Module;
 import fr.aelion.streamer.entities.ModuleToMedia;
+import fr.aelion.streamer.repositories.MediaRepository;
+import fr.aelion.streamer.repositories.ModuleRepository;
+import fr.aelion.streamer.repositories.ModuleToMediaRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,5 +19,62 @@ import java.util.List;
 
 @Service
 public class ModuleService {
+
+    @Autowired
+    private ModuleRepository repository;
+    @Autowired
+    private MediaRepository mediaRepository;
+    @Autowired
+    private ModuleToMediaRepository moduleToMediaRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    public List<ModuleDto> findAll() {
+        List<Module> modules = repository.findAll();
+        List<ModuleDto> modulesDto = modules.stream().map(s->{
+            ModuleDto moduleDto = modelMapper.map(s, ModuleDto.class);
+            return moduleDto;
+        }).toList();
+        return modulesDto;
+    }
+
+    public Module findOne(int id) {
+        return repository.findById(id)
+                .map(s -> s)
+                .orElseThrow();
+    }
+
+    public ModuleDto add(ModuleDto module) {
+        var newModule = new Module();
+        newModule.setName(module.getName());
+        newModule.setObjective(module.getObjective());
+
+        newModule = repository.save(newModule);
+
+        if (module.getMedias().size() > 0) {
+            Module finalNewModule = newModule;
+            List<ModuleToMedia> moduleMedias = new ArrayList<>();
+
+            int i = 0;
+            for (MediaDto mDto : module.getMedias()) {
+                Media newMedia = modelMapper.map(mDto, Media.class);
+                //creer le media
+                newMedia = mediaRepository.save(newMedia);
+
+                //creer la table lien entre le module et chaques medias
+                ModuleToMedia moduleToMedia = new ModuleToMedia();
+                moduleToMedia.setModule(newModule);
+                moduleToMedia.setMedia(newMedia);
+                moduleToMedia.setOrderMedia(i);
+                moduleToMediaRepository.save(moduleToMedia);
+
+                i++;
+
+            }
+
+        }
+        return modelMapper.map(newModule, ModuleDto.class);
+    }
 
 }
